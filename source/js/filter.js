6e315b91845ea9b0344cfa16dce130781fd5edac
SayZ1X <142060056+SayZ1X@users.noreply.github.com>
2023-09-29 17:19:37 +0300

    Add files via upload

--- source/js/filter.js
+++ source/js/filter.js
@@ -1,41 +1,50 @@
-const list = document.querySelector(".filter-menu"),
-  items = document.querySelectorAll(".catalog__card"),
-  listItems = document.querySelectorAll(".filter-menu__button");
+const list = document.querySelector('.filter-menu'),
+	items = document.querySelectorAll('.catalog__card'),
+	listItems = document.querySelectorAll('.filter-menu__button')
 
 function filter() {
-  list.addEventListener("click", (event) => {
-    const targetId = event.target.dataset.id;
-    const target = event.target;
+	list.addEventListener('click', (event) => {
+		const targetId = event.target.dataset.id
+		const target = event.target
 
-    listItems.forEach((listItem) => listItem.classList.remove("filter-selected"));
-    target.classList.add("filter-selected");
+		switch (targetId) {
+			case 'all':
+				getItem('catalog__card')
+				break
+			case 'sites':
+				getItem(targetId)
+				break
+			case 'applications':
+				getItem(targetId)
+				break
+			case 'design':
+				getItem(targetId)
+				break
+			case 'marketing':
+				getItem(targetId)
+				break
+		}
 
-    switch (targetId) {
-      case "all":
-        getItem("catalog__card");
-        break;
-      case "sites":
-        getItem(targetId);
-        break;
-      case "applications":
-        getItem(targetId);
-        break;
-      case "design":
-        getItem(targetId);
-        break;
-      case "marketing":
-        getItem(targetId);
-        break;
-    }
-  });
+		toggleFilterClass(target)
+	})
 }
 
-filter();
+filter()
+
+function toggleFilterClass(selectedButton) {
+	listItems.forEach((btn) => {
+		if (btn === selectedButton) {
+			btn.classList.add('filter-selected')
+		} else {
+			btn.classList.remove('filter-selected')
+		}
+	})
+}
 
 function getItem(className) {
-  items.forEach((item) => {
-    if (item.classList.contains(className)) {
-      item.style.display = "block";
-    } else item.style.display = "none";
-  });
+	items.forEach((item) => {
+		if (item.classList.contains(className)) {
+			item.style.display = 'block'
+		} else item.style.display = 'none'
+	})
 }
